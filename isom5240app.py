0c96374fa3b7b6971255bee90332a7c07a0246c3
KT115 <kktlau115@gmail.com>
2026-09-19 15:02:33 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -12,7 +12,7 @@ from transformers import pipeline
 st.set_page_config(page_title="Deep Learning Sentiment Analysis App", page_icon="💩", layout="centered")
 
 st.title("💩 Sentiment Analysis Tool💩")
-st.write("Enter text below to analyze its sentiment using Hugging Face Transformers.")
+st.write("比卡超見記者")
 
 # Cache the pipeline so the model loads only once
 @st.cache_resource
